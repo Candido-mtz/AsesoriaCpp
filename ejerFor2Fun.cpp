fcf9be76c99cc351bdaaebeff8b0c875a871bec6
Candido Mtz <candido.mtz.rz@gmail.com>
2015-08-20 06:14:40 -0500

    Ejercio con arreglos, fors y funciones
    El programa realizado realiza las siguientes funciones
        * Pide valores para llenar un arreglo (todos los valores).
        * Muestra el contenido del arreglo.
        * Calcula el cuadrado de cada termino del arreglo y lo guarda
          sobre si mismo.
        * Muestra el resultado final de la operacion.

---
+++ ejerFor2Fun.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Se declara un vector de longitud 3 y tipo de dato int
+    int vector[3];
+
+    // Pidiendo valores
+    cout << "Dame los valores del vector" << endl;
+    cout << "Dame el valor 1" << endl;
+    cin >> vector[0];
+    cout << "Dame el valor 2" << endl;
+    cin >> vector[1];
+    cout << "Dame el valor 3" << endl;
+    cin >> vector[2];
+
+    // mostrado el vector
+    cout << "[" ;
+    cout << vector[0] ;
+    cout << "," << vector[1] 
+    cout << "," << vector[2] 
+    cout << "]" << endl;
+
+    // calculando valores
+    c[0] = c[0] * c[0];
+    c[1] = c[1] * c[1];
+    c[2] = c[2] * c[2];
+
+    // mostrando el resultado
+    cout << "[" ;
+    cout << vector[0] ;
+    cout << "," << vector[1] 
+    cout << "," << vector[2] 
+    cout << "]" << endl;
+
+}
+
