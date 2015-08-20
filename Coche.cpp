f39e3d44d8ed11fce3d34d990de235ced6dbb6f7
Candido Mtz <candido.mtz.rz@gmail.com>
2015-08-20 10:40:40 -0500

    Ejercicio 3: Coche
    El programa propuesto intenta simular de forma muy
    rudimentaria el "funcionamiento" de un coche, mo--
    delando unicamente la relacion entre el tanque de
    gasolina y los kilometros recorridos.
    
    El problema se ataca desde un punto de vista es---
    tructural.

---
+++ Coche.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int litros = 10;
+    int lXKm = 1;
+    int kilometraje= 0;
+    int maxTanque = 20;
+
+    bool encendido = false;
+    int opcion;
+
+    while(true){
+        cout << "EDO-> l:" << litros << " kms:" << kilometraje << " enc:" << encendido << endl;
+        cout << " Elije la opcion " << endl;
+        cout << "1: Avanzar" << endl;
+        cout << "2: Llenar tanque" << endl;
+        cout << "3: Encender" << endl;
+        cout << "4: Apagar" << endl;
+        cin >> opcion;
+
+        switch (opcion){
+            case 1:
+                cout << "Dame cuantos kilometros " << endl;
+                cin >> opcion;
+                kilometraje += opcion;
+                litros -= opcion * lXKm;
+                break;
+            case 2:
+                litros = maxTanque;
+                break;
+            case 3:
+                if( encendido)
+                    cout << "Error, ya esta encendido" << endl;
+                else
+                    encendido = true;
+                break;
+            case 4:
+                if ( encendido)
+                    encendido = false;
+                else
+                    cout << "Ya esta apagado" << endl;
+                break;
+        }
+    }
+    return 0;
+}
